fca1127939abb7cd012d04c30afae8dbc4e88753
Wayne Crouch <wgcrouch@gmail.com>
2010-07-22 14:06:56 +0100

    Added random line and equations for graphs

--- index.php
+++ index.php
@@ -68,15 +68,23 @@
 								return 1/x;
 							}
 						}
+					},
+					'random' : {
+						color : '#F0F',
+						formula : {
+							calculateY : function(x) {
+								return Math.random() * 5;
+							}
+						}
 					}
  				}
 
 				var graph = new Graph(settings);
-//
-//				//graph.addLine()
+
 				graph.drawAxes();
 
 				
+
 
 				jQuery('input[name^=checkbox]').click(function(){
 					var type = jQuery(this).val();
@@ -107,13 +115,14 @@
 				
 				Zoom: <input id ="zoom-level" type="range"  min="1" max="80"  value ="20" default="20" /><br/>
 
-				<label><input type="checkbox" name="checkbox-straight"  id="checkbox-straight"  value="straight"/> Straight Line</label><br/>
-				<label><input type="checkbox" name="checkbox-quadratic" id="checkbox-quadratic" value="quadratic"/> Quadratic Curve</label><br/>
-				<label><input type="checkbox" name="checkbox-cubic"     id="checkbox-cubic"     value="cubic"/> Cubic Curve</label><br/>
-				<label><input type="checkbox" name="checkbox-circle"    id="checkbox-circle"    value="circle"/> Half Circle</label><br/>
-				<label><input type="checkbox" name="checkbox-sine"      id="checkbox-sine"      value="sine"/> Sine Graph</label><br/>
-				<label><input type="checkbox" name="checkbox-tan"       id="checkbox-tan"       value="tan"/> Tan Graph</label><br/>
-				<label><input type="checkbox" name="checkbox-oneoverx"  id="checkbox-oneoverx"  value="oneoverx"/> 1/x</label><br/>
+				<label><input type="checkbox" name="checkbox-straight"  id="checkbox-straight"  value="straight"/> Straight Line (y = mx+c)</label><br/>
+				<label><input type="checkbox" name="checkbox-quadratic" id="checkbox-quadratic" value="quadratic"/> Quadratic Curve (y = ax^2 + bx + c)</label><br/>
+				<label><input type="checkbox" name="checkbox-cubic"     id="checkbox-cubic"     value="cubic"/> Cubic Curve (y = ax^3 + bx^2 +cx +d)</label><br/>
+				<label><input type="checkbox" name="checkbox-circle"    id="checkbox-circle"    value="circle"/> Half Circle (x^2 + y^2 = r^2)</label><br/>
+				<label><input type="checkbox" name="checkbox-sine"      id="checkbox-sine"      value="sine"/> Sine Graph (y = sin(x))</label><br/>
+				<label><input type="checkbox" name="checkbox-tan"       id="checkbox-tan"       value="tan"/> Tan Graph (y = tan(x))</label><br/>
+				<label><input type="checkbox" name="checkbox-oneoverx"  id="checkbox-oneoverx"  value="oneoverx"/> y = 1/x</label><br/>
+				<label><input type="checkbox" name="checkbox-random"    id="checkbox-random"  value="random"/> Random Points</label><br/>
 			</fieldset>
 		</div>
     </body>
